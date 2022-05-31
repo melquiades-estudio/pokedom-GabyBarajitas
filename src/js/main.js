b674f80320b194a45e1761ee2b73b61acbed6c38
GabyBarajitas <gabybarajasbcn@gmail.com>
2022-05-31 21:57:30 +0200

    funcion newNode validada

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,43 @@
 import pokemons from "./data.js";
+//var concatNodes =``;
 
-function creaNodo()
-
-
-function contenidoNodo(newImg,newName,newGras){
-    let imagen = newImg;
+function newNode(newImg,newName,newType){
+    let image = newImg;
     let nombre = newName;
-    let varGras = newGras;
-    let posicion = document.querySelector(".list-pokemons");
-
-    posicion.innerHTML = `
-    <article class="list-pokemons-item">
-          <div class="list-pokemons-item-content">
-            <img
-              src= ${imagen}
-              alt="pokemon-1"
-            />
-            <h3>
-              ${nombre}
-              <span>${varGras}</span>
-            </h3>
-          </div>
-        </article>`
+    let varType = newType;
+    let nodoNuevo =
+    `<article class="list-pokemons-item">
+        <div class="list-pokemons-item-content">
+          <img src= ${image} alt="pokemon-1"/>
+            <h3>${nombre}<span>${varType}</span></h3>
+         </div>
+      </article>`;
+      return nodoNuevo;
 }
 
-contenidoNodo('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',"bulbasaur1","water");
-contenidoNodo('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',"bulbasaur2","water");
-contenidoNodo('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',"bulbasaur3","water");
+function insertNode(nodeInfo){
+  let posicion = document.querySelector(".list-pokemons");
+  posicion.innerHTML = nodeInfo;
+}
+
+function main(){
+  let concatNodes = ``;
+  let imagenPoke;
+  let nombrePoke;
+  let tipoPoke;
+  let sizeArr;
+  let i = 0;
+
+  sizeArr = pokemons.length;
+  console.log(pokemons);
+  console.log(sizeArr);
+
+  console.log(concatNodes);
+  concatNodes = concatNodes + newNode('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',"bulbasaur1","water");
+  console.log(concatNodes);
+  concatNodes = concatNodes + newNode('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',"bulbasaur1","water");
+  console.log(concatNodes);
+}
+
+main()
 
